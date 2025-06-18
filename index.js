036e3e73d724e6ffb58ad82d7b28ef4a583f485c
25HP8413 25HP8413 <25HP8413@G301-CC0188.local>
2025-06-18 14:42:05 +0800

    to-do

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const number1 = document.getElementById("number1");
 const number2 = document.getElementById("number2");
+const operations2 = document.getElementById("operations2");
+const number3 = document.getElementById("number3");
 const operations = document.getElementById("operations");
 const resultDiv = document.getElementById("result");
 
 function operation() {
-  const num1 = +number1.value;
-  const num2 = +number2.value;
+  const num1 = Number(number1.value);
+  const num2 = Number(number2.value);
   const op = operations.value;
+  const num3 = Number(number3.value);
+  const op2 = operations2.value;
 
   let result;
 
@@ -19,6 +23,16 @@ function operation() {
   } else if (op === "/") {
     result = num1 / num2;
   }
+  let result2;
+  if (op2 === "+") {
+    result2 = result + num3;
+  } else if (op2 === "-") {
+    result2 = result - num3;
+  } else if (op2 === "*") {
+    result2 = result * num3;
+  } else if (op2 === "/") {
+    result2 = result / num3;
+  }
 
-  resultDiv.innerText = result;
+  resultDiv.innerText = `result: ${result2}`;
 }
